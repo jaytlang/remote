5a019a0ea18e95ccb6898bfdead5c6e6385832bd
Jay Lang <jaytlang@mit.edu>
2023-04-05 14:06:31 -0400

    first commit

---
+++ conf.py
@@ -0,0 +1,9 @@
+# Configure me!
+
+client_cert = "remote/client.pem"
+client_key = "remote/client.key"
+client_ca = "remote/mitcca.pem"
+server_ca = "remote/serverca.pem"
+
+server_hostname = "eecs-digital-51.mit.edu"
+server_port = 443
