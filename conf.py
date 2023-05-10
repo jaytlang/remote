7256b2efb2adc227951633ccee8aef2bbea79760
Jay Lang <jaytlang@mit.edu>
2023-05-10 16:53:44 -0400

    no more hardcoding serverca

--- conf.py
+++ conf.py
@@ -3,7 +3,6 @@
 client_cert = "remote/client.pem"
 client_key = "remote/client.key"
 client_ca = "remote/mitcca.pem"
-server_ca = "remote/serverca.pem"
 
 server_hostname = "eecs-digital-51.mit.edu"
 server_port = 443
